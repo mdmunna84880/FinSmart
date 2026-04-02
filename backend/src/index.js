968827c65da466f470c886e0a8fe7e3dfa4cdccb
mdmunna84880 <mdmunna19434@gmail.com>
2026-04-02 08:05:33 +0530

    Set dns it can get connected to mongodb safely

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,10 @@
 import app from "./app.js";
 import connectDB from "./config/db.js";
 import env from "./config/env.js";
+import dns from "dns";
+
+// Setting dns to this specific so that it don't break while connecting to MONGODB
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const PORT = env.PORT;
 // Connect to db
